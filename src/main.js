4a16185d106419cf10c89029d3563308cf50f951
tgnlex <alexanderwebber76@gmail.com>
2023-12-16 16:00:14 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,15 +2,9 @@ let app = document.querySelector('#app')
 let heading = document.createElement('h1');
 let heading_text = document.createTextNode('My Quill Editor')
 
-let div = document.createElement('div')
-
+let div = document.createElement('div');
+let editor = div.setAttribute("id", 'editor');
 heading.appendChild(heading_text)
-app.prepend(heading);
-app.insertAdjacentHTML(
-  afterBegin, 
-  `
-    <div id="editor><p>Hello World from JS</p></div>
-    <div id="toolbar"></div>
-  `
-)
+app.prepend(heading)
+app.appendChild(editor)
 
