4e957b821d1540c80368a2d3c6e14e8c4bdb9717
tgnlex <alexanderwebber76@gmail.com>
2023-12-20 14:56:31 -0800

    small fix

--- src/main.js
+++ src/main.js
@@ -6,5 +6,5 @@ let div = document.createElement('div');
 let editor = div.setAttribute("id", 'editor');
 heading.appendChild(heading_text)
 app.prepend(heading)
-app.appendChild(editor)
+app.(editor)
 
